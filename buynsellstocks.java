2112075d11511ab064088eeeacef6075da4d274c
aman-102004 <amanvish7870@gmail.com>
2026-03-26 15:37:09 +0530

    commit

---
+++ buynsellstocks.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class buynsellstocks {
+
+    public static int buynsellstocks(int prices[]){
+        int buyprice=Integer.MAX_VALUE;
+        int maxprofit=0;
+
+        for(int i= 0;i<prices.length;i++){
+            if(buyprice<prices[i]){
+                int profit=prices[i]-buyprice;//todays profit
+                maxprofit=Math.max(maxprofit, profit);
+            
+            }
+            else{
+                buyprice=prices[i];
+            }
+        }
+        return maxprofit;
+
+    }
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        System.out.println(buynsellstocks(prices));
+
+    }
+    
+}
