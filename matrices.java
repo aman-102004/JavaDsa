0481e4885484b203f86f15462735067579b027b1
aman-102004 <amanvish7870@gmail.com>
2026-03-31 16:11:46 +0530

    commit

--- matrices.java
+++ matrices.java
@@ -1,5 +1,19 @@
+import java.net.SocketTimeoutException;
 import java.util.* ;
 public class matrices {
+
+    public static boolean search(int matrix[][],int key){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+               if(matrix[i][j]==key){
+                System.out.println("found at cell ("+i+","+j +")");
+                return true;
+               }
+            }
+        }
+        System.out.println("Key Not found");
+        return false;
+    }
     public static void main(String[] args) {
         int matrix[][]=new int[3][3];
         int n=matrix.length;//for no of rows;
@@ -16,7 +30,9 @@ public class matrices {
                 System.out.print(matrix[i][j]+" ");
             }
             System.out.println();
+
         }
+        search(matrix,5);
     }
     
 }
