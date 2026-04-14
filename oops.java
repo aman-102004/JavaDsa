c3b8d8c8da64259f3e888bbd6a02019938a5d0f4
aman-102004 <amanvish7870@gmail.com>
2026-04-14 16:03:36 +0530

    commit

--- oops.java
+++ oops.java
@@ -2,23 +2,48 @@ public class oops {
     public static void main(String[] args) {
         Pen p1=new Pen();//created a pen object called p1
         p1.setcolor("Blue");
-        System.out.println(p1.color);
+        System.out.println(p1.getcolor());
+        p1.settip(7);
+        System.out.println(p1.gettip());
+
+        // Bankaccount myacc=new Bankaccount();
+        // myacc.Username="Aman";
+        // myacc.setpassword("Amanv");
+        
     }
     
 }
 
+// class Bankaccount{
+
+//     public String Username;
+//     private String password;
+
+//     public void setpassword(String pwd){
+//         password=pwd;
+//     }
+
+// }
+
 class Pen{
     //prop +func
 
-    String color;
-    int tip;
+    private String color;
+    private int tip;
 
+    String getcolor(){
+        return this.color;
+    }
+
+    int gettip(){
+        return this.tip;
+    }
     void setcolor(String newColor){
-        color=newColor;
+        this.color=newColor;
         
     }
     void settip(int newTip){
-        tip=newTip;
+        this.tip=newTip;
         
     }
 
