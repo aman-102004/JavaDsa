354b8fc870d290cf82f718693aea706b9db9dfb2
aman-102004 <amanvish7870@gmail.com>
2026-05-02 16:32:29 +0530

    commit

--- backtracking.java
+++ backtracking.java
@@ -1,8 +1,31 @@
 public class backtracking{
+    public static void printarray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 
+    public static void changearr(int arr[],int i,int v){
+        //base case
+        if(i==arr.length){
+            printarray(arr);
+            return;
+
+        }
+
+        //recursion part
+        arr[i]=v;
+        changearr(arr, i+1, v+1);//function call step
+        arr[i]=arr[i]-2;//backtracking step
+        
+
+    }
 
     public static void main(String[] args) {
-        int num;
+        int arr[]=new int[5];
+        changearr(arr, 0, 1);
+        System.out.println();
+        printarray(arr);
     }
 }
     
