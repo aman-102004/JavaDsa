4d6ea87e3fb3e008318e55ad5dac9f81eaf2619f
aman-102004 <amanvish7870@gmail.com>
2026-05-16 18:56:56 +0530

    commit

--- LinkedList.java
+++ LinkedList.java
@@ -268,49 +268,189 @@ public class LinkedList {
 
     }
 
+    public  boolean isCycle(){//this method is called floyd cycle finding algo
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast){
+                return true;
+            }
+
+            
+
+            }
+            return false;
+    }
+
+    public void removecycle(){
+        //detect cycle
+        Node slow=head;
+        Node fast=head;
+
+        boolean cycle =false;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(fast==slow){
+                cycle=true;
+                break;
+            }
+        }
+
+        if(cycle==false){
+            return;
+        }
+        //find meeting point
+
+        slow=head;
+        Node prev=null;
+
+        while(slow!=fast){
+            prev=fast;
+            slow=slow.next;
+            fast=fast.next;
+
+        }
+
+
+        
+        //remove cycle
+        prev.next=null;
+    }
+
+    public Node getMid(Node head){
+        Node slow=head;
+        Node fast=head.next;
+
+        while(fast!=null &&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
+    }
+
+    public Node merge(Node head1,Node head2){
+        Node mergedLinked=new Node(-1);
+        Node temp=mergedLinked;
+
+
+        while(head1!=null && head2!=null){
+            if(head1.data<=head2.data){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }else{
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        }
+        while(head1!=null){
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+
+        }
+
+        while(head2!=null){
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+
+        }
+        return mergedLinked.next;
+    }
+
+    public Node mergeSort(Node head){//O(nlogn)
+        if(head==null || head.next==null){
+            return head;
+        }
+        //find mid
+        Node mid=getMid(head);
+
+        //left and right mergeSort
+        Node righthead=mid.next;
+        mid.next=null;
+
+        Node newLeft=mergeSort(head);
+        Node newRight=mergeSort(righthead);
+
+        //merge
+        return merge(newLeft,newRight);
+    }
+
+
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
 
-        // l1.head=new Node(1);
-        // l1.head.next=new Node(2);
-        //we will be using methods for adding and every operations unlike the above manual methods
+        // // l1.head=new Node(1);
+        // // l1.head.next=new Node(2);
+        // //we will be using methods for adding and every operations unlike the above manual methods
 
+        // ll.addFirst(2);
+        // ll.addFirst(1);//O(n) to add a num
+
+        // ll.addLast(3);
+        // ll.addLast(4);//O(1);
+
+        // ll.addinmiddle(2, 9);
+        
+        
+        // ll.printData();
+        // System.out.println(ll.size);
+
+        // ll.removeFirst();
+
+        // ll.printData();
+        
+
+        // ll.removeLast();
+
+        // ll.printData();
+
+        // System.out.println(ll.SearchIterative(10));
+
+        // System.out.println(ll.SearchRecursive(3));
+
+        // ll.reverse();
+
+        // ll.printData();
+
+        // ll.addFirst(7);
+        // ll.addFirst(10);
+        // ll.printData();
+
+        // ll.deleteNthfromend(2);
+        // ll.printData();
+        // System.out.println(ll.checkPalindrome());
+
+        // head=new Node(1);
+        // Node temp=new Node(2);
+        // head.next=temp;
+        // head.next.next=new Node(3);
+        // head.next.next.next=temp;
+        // //1->2->-3->1
+
+        // System.out.println(ll.isCycle());
+        // ll.removecycle();
+        // System.out.println(ll.isCycle());
+
+        ll.addFirst(1);
         ll.addFirst(2);
-        ll.addFirst(1);//O(n) to add a num
-
-        ll.addLast(3);
-        ll.addLast(4);//O(1);
-
-        ll.addinmiddle(2, 9);
-        
-        
-        ll.printData();
-        System.out.println(ll.size);
-
-        ll.removeFirst();
-
-        ll.printData();
-        
-
-        ll.removeLast();
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
 
         ll.printData();
 
-        System.out.println(ll.SearchIterative(10));
-
-        System.out.println(ll.SearchRecursive(3));
-
-        ll.reverse();
+        ll.head=ll.mergeSort(head);
 
         ll.printData();
 
-        ll.addFirst(7);
-        ll.addFirst(10);
-        ll.printData();
-
-        ll.deleteNthfromend(2);
-        ll.printData();
-        System.out.println(ll.checkPalindrome());
     }
     }
     
