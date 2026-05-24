fcd4e1eded28df657340cc12da8b61833cbd537d
aman-102004 <amanvish7870@gmail.com>
2026-05-24 15:55:23 +0530

    commit

--- stack.java
+++ stack.java
@@ -220,6 +220,60 @@ public class stack {
         return false;
     }
 
+    public static void maxAreaRectangle(int arr[]){
+        int maxarea=0;
+        int nsr[]=new int [arr.length];
+        int nsl[]=new int[arr.length];
+        
+        //next smaller right
+        Stack<Integer> s=new Stack<>();
+
+        for(int i=arr.length-1;i>=0;i--){
+            while(!s.isEmpty()&&arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsr[i]=arr.length;
+            }else{
+                nsr[i]=s.peek();
+            }
+            s.push(i);
+        }
+        
+
+
+        //next smaller left
+        s=new Stack<>();
+
+        for(int i=0;i<arr.length;i++){
+            while(!s.isEmpty()&&arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsl[i]=-1;
+            }else{
+                nsl[i]=s.peek();
+            }
+            s.push(i);
+        }
+
+
+        //current area
+        //width=j-i-1=nsr[i]-nsl[i]-1
+        //height=length of bar
+
+        for(int i=0;i<arr.length;i++){
+            int height=arr[i];
+            int width=nsr[i]-nsl[i]-1;
+            int currarea=height*width;
+            maxarea=Math.max(currarea,maxarea);
+        }
+
+        System.out.println("Max area of histogram is "+ maxarea);
+
+
+    }
+
     public static void main(String[] args) {
         //we can create stack using prebuilt stack library 
         //Stack<Integer>s=new Stack<>();
@@ -310,11 +364,16 @@ public class stack {
 
     //Duplicate Parenthesis
 
-    String str="((a+b))";
-    String str2="(a-b)";
+    // String str="((a+b))";
+    // String str2="(a-b)";
 
-    System.out.println(ParenthesisIsDuplicate(str2));
-    System.out.println(ParenthesisIsDuplicate(str));
+    // System.out.println(ParenthesisIsDuplicate(str2));
+    // System.out.println(ParenthesisIsDuplicate(str));
+    
+
+    int arr[]={2,1,5,6,2,3};
+    maxAreaRectangle(arr);
+
 
 
 
