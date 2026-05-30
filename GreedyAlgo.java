27454b224113213817a711cc9632d00e6ed93493
aman-102004 <amanvish7870@gmail.com>
2026-05-30 18:03:14 +0530

    commit

--- GreedyAlgo.java
+++ GreedyAlgo.java
@@ -7,18 +7,29 @@ public class GreedyAlgo {
         int end[]={2,4,6,7,9,9};
         //end time sorted basis
 
+        //if it was not sorted
+        int activities[][]=new int[start.length][3];
+
+        for(int i=0;i<start.length;i++){
+            activities[i][0]=i;
+            activities[i][1]=start[i];
+            activities[i][2]=end[i];
+        }
+        //Lambda functions to sort 2d arrays
+        //this indicates sorting on the bases of the index 2
+        Arrays.sort(activities,Comparator.comparingDouble(o -> o[2]));
         int maxAct=0;
 
         ArrayList<Integer> ans=new ArrayList<>();
 
         maxAct=1;
-        ans.add(0);
-        int lastend=end[0];
+        ans.add(activities[0][0]);
+        int lastend=activities[0][2];
         for(int i=1;i<end.length;i++){
-            if(start[i]>=lastend){
+            if(activities[i][1]>=lastend){
                 maxAct++;
-                ans.add(i);
-                lastend=end[i];
+                ans.add(activities[i][0]);
+                lastend=activities[i][2];
             }
 
         }
