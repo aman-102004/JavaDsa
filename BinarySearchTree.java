2ddb0febb09a2d48056359e7d17d9b68dd035539
aman-102004 <amanvish7870@gmail.com>
2026-06-10 09:27:12 +0530

    commit

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -57,8 +57,49 @@ public class BinarySearchTree {
 
     }
 
+    public static Node delete(Node root,int val){
+        if(root.data<val){
+            root.right=delete(root.right, val);
+
+        }
+        else if(root.data>val){
+            root.left=delete(root.left, val);
+        }
+        else{
+            //case 1-left node
+
+            if(root.left==null && root.right==null){
+                return null;
+            }
+
+            //case 2-single child
+            if(root.left==null){
+                return root.right;
+            }
+
+            if(root.right==null){
+                return root.left;
+            }
+
+            //case 3-both children
+            Node IS=findInorderSuccessor(root.right);
+            root.data=IS.data;
+            root.right=delete(root.right,IS.data);
+        }
+
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while(root.left!=null){
+            root=root.left;
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
-        int values[]={5,1,3,4,2,7};
+        int values[]={8,5,3,1,4,6,10,11,14};
         Node root=null;
 
         for(int i=0;i<values.length;i++){
@@ -69,6 +110,9 @@ public class BinarySearchTree {
 
         System.out.println();
         System.out.println(Search(root, 8));
+
+        delete(root, 1);
+        inorder(root);
     }
     
 }
