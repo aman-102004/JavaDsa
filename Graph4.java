c4d6e10bc8ece7ee6bf2e91c7f82b0f33c9aef84
aman-102004 <amanvish7870@gmail.com>
2026-07-06 11:56:47 +0530

    commit

--- Graph4.java
+++ Graph4.java
@@ -1,5 +1,89 @@
-public class Graph4 {
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
+public class Graph4 {
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s,int d,int w){
+            this.src=s;
+            this.dest=d;
+            this.wt=w;
+        }
+    }
+
+    public static void createGraph(int flights[][],ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<>();
+        }
+        for(int i=0;i<flights.length;i++){
+            int src=flights[i][0];
+            int dest=flights[i][1];
+            int wt=flights[i][2];
+
+            Edge e=new Edge(src,dest,wt);
+            graph[src].add(e);
+
+        }
+    }
+    static class Info{
+        int v;
+        int cost;
+        int stops;
+
+        public Info(int v,int c,int s){
+            this.v=v;
+            this.cost=c;
+            this.stops=s;
+        }
+
+    }
+    public static int cheapestflight(int n,int flights[][],int src,int dest,int k){
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[]=new ArrayList[n];
+        createGraph(flights, graph);
+
+        //performing djistra
+        int dist[]=new int[n];
+        for(int i=0;i<n;i++){
+            if(i!=src){
+                dist[i]=Integer.MAX_VALUE;
+            }
+        }
+        @SuppressWarnings("unchecked")
+        Queue<Info> q=new LinkedList();
+        q.add(new Info(src,0,0));
+
+        while(!q.isEmpty()){
+            Info curr=q.remove();
+            if(curr.stops>k){
+                break;
+            }
+            for(int i=0;i<graph[curr.v].size();i++){
+                Edge e=graph[curr.v].get(i);
+                int u=e.src;
+                int v=e.dest;
+                int wt=e.wt;
+                //dist[u]!=Integer.MAX_VALUE We need to check this so that when we add or sbtract something it does not give some unknown value
+                if(curr.cost+wt<dist[v] && curr.stops<=k){
+                    dist[v]=curr.cost+wt;
+                    q.add(new Info(v,dist[v],curr.stops+1));
+                }
+            }
+        }
+
+        //dest[dist]
+
+        if(dist[dest]==Integer.MAX_VALUE){
+            return -1;
+        }else{
+            return dist[dest];
+        }
+
+    }
     public static void main(String[] args) {
         //CHEAPEST FLIGHT WITH K STOPS
         //WE HAVE TO REACH THE DEST IN K STOPS AND CONSIDERIGN THE MINIMUM COST
@@ -7,6 +91,11 @@ public class Graph4 {
         int n=4;
         int flights[][]={{0,1,100},{1,2,100},{2,0,100},{1,3,600},{2,3,200}};
         int src=0,dest=3,k=1;
+        
+
+        System.out.println(cheapestflight(n, flights, src, dest, k));
+
+
     }
     
 }
