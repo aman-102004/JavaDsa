2c0e0b90c9390fe2de078f89a1b0a120371e3b95
aman-102004 <amanvish7870@gmail.com>
2026-07-25 20:59:18 +0530

    commit

--- DP2.java
+++ DP2.java
@@ -206,6 +206,84 @@ public class DP2 {
 
         //dp[i][j] means what will be the max profit at i items and j max weight and store that at dp[i][j]
     }
+
+    public static int rodCutting(int length[],int price[],int rodlength){
+
+        int dp[][]=new int[length.length+1][rodlength+1];
+        //dp[i][j]=can we make a rod of length x using the given pieces(prices)
+        for(int i=0;i<price.length;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<rodlength;i++){
+            dp[0][i]=0;
+        }
+
+        for(int i=1;i<length.length+1;i++){
+            for(int j=1;j<rodlength+1;j++){
+                if(length[i-1]<=j){
+                    dp[i][j]=Math.max(price[i-1]+dp[i][j-length[i-1]],dp[i-1][j]);
+
+                }else{
+                   dp[i][j]=dp[i-1][j]; 
+                }
+            }
+        }
+        return dp[length.length][rodlength];
+
+
+
+    }
+
+    public static int lcs(String str1,String str2,int n,int m){
+        if(n==0 || m==0){
+            return 0;
+        }
+
+        if(str1.charAt(n-1)==str2.charAt(m-1)){
+            return lcs(str1,str2,n-1,m-1)+1;
+        }else{
+            int ans1=lcs(str1,str2,n-1,m);
+            int ans2=lcs(str1,str2,n,m-1);
+
+            return Math.max(ans1,ans2);
+        }
+    }
+
+     public static int lcsMemoization(String str1,String str2,int n,int m,int dp[][]){
+        if(n==0 || m==0){
+            return 0;
+        }
+
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+
+        if(str1.charAt(n-1)==str2.charAt(m-1)){
+            return dp[n][m]=lcsMemoization(str1,str2,n-1,m-1,dp)+1;
+        }else{
+            int ans1=lcsMemoization(str1,str2,n-1,m,dp);
+            int ans2=lcsMemoization(str1,str2,n,m-1,dp);
+
+            return dp[n][m]=Math.max(ans1,ans2);
+        }
+    }
+
+    public static int lcsTabulation(String str1,String str2,int dp[][],int n,int m){
+        //here dp[i][j]= is there any subsequnce of characters in string of size i and j
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    int ans1=dp[i-1][j];
+                    int ans2=dp[i][j-1];
+                    dp[i][j]=Math.max(ans1,ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    
     public static void main(String[] args) {
         //ClIMBING STAIRS
         // System.out.println(ClimbingStairsRecursion(5));
@@ -246,18 +324,61 @@ public class DP2 {
 
 
 
-        //Target Sum
-        int arr[]={4,2,7,1,3};
-        int sum=10;
-        System.out.println(targetSumSubsetTabulation(arr, sum));
+        // //Target Sum
+        // int arr[]={4,2,7,1,3};
+        // int sum=10;
+        // System.out.println(targetSumSubsetTabulation(arr, sum));
 
 
         //unbounded knapsack
-        int val[]={15,14,10,45,30,18};
-        int wt[]={2,5,1,3,4,6};
-        int W=7;
-        System.out.println(UnboundedKnapsackTabulation(val,wt,W));
+        // int val[]={15,14,10,45,30,18};
+        // int wt[]={2,5,1,3,4,6};
+        // int W=7;
+        // System.out.println(UnboundedKnapsackTabulation(val,wt,W));
 
+
+        //Rod Cutting
+        // int length[]={1,2,3,4,5,6,7,8};
+        // int price[]={1,5,8,9,10,17,17,20};
+        // int rodlength=8;
+        // System.out.println(rodCutting(length, price, rodlength));
+
+        //LCS(LARGEST COMMON SUBSEQUENCE)
+
+        // String str1="abcdge";
+        // String str2="abedg";
+        // System.out.println(lcs(str1,str2,str1.length(),str2.length()));
+
+        // //LCS MEMOIZATION 
+        // int dp[][]=new int[str1.length()+1][str2.length()+1];
+
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[0].length;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+
+        // System.out.println(lcsMemoization(str1, str2,str1.length(),str2.length(), dp));
+
+
+
+        //LCS TABULATION
+        String str1="abcdge";
+        String str2="abedg";
+
+        int dp[][]=new int[str1.length()+1][str2.length()+1];
+
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=0;
+
+        }
+
+        for(int i=0;i<dp[0].length;i++){
+            dp[0][i]=0;
+
+        }
+
+        System.out.println(lcsTabulation(str1, str2, dp,str1.length(),str2.length()));
 
     }
 }
