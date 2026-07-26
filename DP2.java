6b50cbe35c776873da19caeb87f1240ce9bac383
aman-102004 <amanvish7870@gmail.com>
2026-07-26 18:11:05 +0530

    commit

--- DP2.java
+++ DP2.java
@@ -283,6 +283,40 @@ public class DP2 {
         }
         return dp[n][m];
     }
+
+
+    //variation of lcs
+    public static int LongestCommonSubstring(String str1,String str2,int n,int m){
+        //table
+        int dp[][]=new int[n+1][m+1];
+        //meaning = for dp[i][j] if string of length i,j is taken what is the length of longest common string bw them
+        
+
+        //initialisation
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
+        }
+
+        for(int i=0;i<m+1;i++){
+            dp[0][i]=0;
+        }
+
+        int ans=0;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    ans=Math.max(ans,dp[i][j]);
+                }else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+
+        return ans;
+       
+
+    }
     
     public static void main(String[] args) {
         //ClIMBING STAIRS
@@ -363,22 +397,31 @@ public class DP2 {
 
 
         //LCS TABULATION
-        String str1="abcdge";
-        String str2="abedg";
+        // String str1="abcdge";
+        // String str2="abedg";
 
-        int dp[][]=new int[str1.length()+1][str2.length()+1];
+        // int dp[][]=new int[str1.length()+1][str2.length()+1];
 
-        for(int i=0;i<dp.length;i++){
-            dp[i][0]=0;
+        // for(int i=0;i<dp.length;i++){
+        //     dp[i][0]=0;
 
-        }
+        // }
 
-        for(int i=0;i<dp[0].length;i++){
-            dp[0][i]=0;
+        // for(int i=0;i<dp[0].length;i++){
+        //     dp[0][i]=0;
 
-        }
+        // }
 
-        System.out.println(lcsTabulation(str1, str2, dp,str1.length(),str2.length()));
+        // System.out.println(lcsTabulation(str1, str2, dp,str1.length(),str2.length()));
+
+
+
+        //Longest common Substring
+        String str1="ABCDE";
+        String str2="ABGCE";
+        int n=str1.length();
+        int m=str2.length();
+        System.out.println(LongestCommonSubstring(str1, str2, n, m));
 
     }
 }
