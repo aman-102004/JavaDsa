69a3f0b7a38c54767524aa06406beea3501c78df
aman-102004 <amanvish7870@gmail.com>
2026-07-29 16:40:09 +0530

    commit

--- DP2.java
+++ DP2.java
@@ -285,38 +285,38 @@ public class DP2 {
     }
 
 
-    //variation of lcs
-    // public static int LongestCommonSubstring(String str1,String str2,int n,int m){
-    //     //table
-    //     int dp[][]=new int[n+1][m+1];
-    //     //meaning = for dp[i][j] if string of length i,j is taken what is the length of longest common string bw them
+    variation of lcs
+    public static int LongestCommonSubstring(String str1,String str2,int n,int m){
+        //table
+        int dp[][]=new int[n+1][m+1];
+        //meaning = for dp[i][j] if string of length i,j is taken what is the length of longest common string bw them
         
 
-    //     //initialisation
-    //     for(int i=0;i<n+1;i++){
-    //         dp[i][0]=0;
-    //     }
+        //initialisation
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
+        }
 
-    //     for(int i=0;i<m+1;i++){
-    //         dp[0][i]=0;
-    //     }
+        for(int i=0;i<m+1;i++){
+            dp[0][i]=0;
+        }
 
-    //     int ans=0;
-    //     for(int i=1;i<n+1;i++){
-    //         for(int j=1;j<m+1;j++){
-    //             if(str1.charAt(i-1)==str2.charAt(j-1)){
-    //                 dp[i][j]=dp[i-1][j-1]+1;
-    //                 ans=Math.max(ans,dp[i][j]);
-    //             }else{
-    //                 dp[i][j]=0;
-    //             }
-    //         }
-    //     }
+        int ans=0;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    ans=Math.max(ans,dp[i][j]);
+                }else{
+                    dp[i][j]=0;
+                }
+            }
+        }
 
-    //     return ans;
+        return ans;
        
 
-    // }
+    }
 
     // public int longestPalindromeSubseq(String s) {
     //     StringBuilder sb=new StringBuilder("");
