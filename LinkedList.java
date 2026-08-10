4889c761951c4d51ae49e2fdea5f120b5321f064
aman-102004 <amanvish7870@gmail.com>
2026-08-10 20:02:27 +0530

    commit

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class LinkedList {
 
     public static class Node{
@@ -428,6 +430,46 @@ public class LinkedList {
 
     }
 
+    public Node addTwoNumbers(Node l1, Node l2) {
+
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        Node ans = null;
+        int carry = 0;
+
+        while (!s1.isEmpty() || !s2.isEmpty() || carry != 0) {
+
+            int sum = carry;
+
+            if (!s1.isEmpty()) {
+                sum += s1.pop();
+            }
+
+            if (!s2.isEmpty()) {
+                sum += s2.pop();
+            }
+
+            carry = sum / 10;
+
+            Node newNode = new Node(sum % 10);
+
+            newNode.next = ans;
+            ans = newNode;
+        }
+
+        return ans;
+    }
 
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
