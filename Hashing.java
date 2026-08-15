1ad0c5c6c1726c6420caa59d8c7aeb779c20d04c
aman-102004 <amanvish7870@gmail.com>
2026-08-15 20:25:08 +0530

    commit

--- Hashing.java
+++ Hashing.java
@@ -63,6 +63,37 @@ public class Hashing {
         }
         return max;
         
+    }   
+
+    //max length of equal nums of 0 and 1 
+
+     public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, -1);
+
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0){
+                sum--;
+            }
+                
+            else{
+                sum++;
+
+            }
+                
+            if (map.containsKey(sum)) {
+                max = Math.max(max, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return max;
     }
     //LEETCODE QUE
     // public static String getStart(HashMap<String,String> tickets){
