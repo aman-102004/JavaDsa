12215e77293cda9ec7a8f51e380456768384193b
aman-102004 <amanvish7870@gmail.com>
2026-08-28 20:37:27 +0530

    commit

--- BinaryTress2.java
+++ BinaryTress2.java
@@ -387,6 +387,45 @@ public class BinaryTress2 {
         sum(root.right,targetsum,answer,temp,currsum);
         temp.remove(temp.size()-1);
     }
+
+    public String multiply(String num1, String num2) {
+
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int[] result = new int[num1.length() + num2.length()];
+
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+
+                int a = num1.charAt(i) - '0';
+                int b = num2.charAt(j) - '0';
+
+                int product = a * b;
+
+                int pos1 = i + j;
+                int pos2 = i + j + 1;
+
+                int sum = product + result[pos2];
+
+                result[pos2] = sum % 10;
+                result[pos1] += sum / 10;
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int digit : result) {
+            if (ans.length() == 0 && digit == 0) {
+                continue;
+            }
+
+            ans.append(digit);
+        }
+
+        return ans.toString();
+    }
     public static void main(String[] args) {
            /*      1
                  /    \
