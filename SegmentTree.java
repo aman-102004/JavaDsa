bbf4a559653481cbbc8abd9636be90c01b0c2a53
aman-102004 <amanvish7870@gmail.com>
2026-09-21 14:17:42 +0530

    commit

--- SegmentTree.java
+++ SegmentTree.java
@@ -18,6 +18,30 @@ public class SegmentTree {
         tree[i]=tree[2*i+1]+tree[2*i+2];
         return tree[i];
     }
+
+    //i==tree index,si sj=start and end index of the sum of ith node,qi and qj are index of the sum reqd
+    public static int getSumUtil(int i,int si,int sj ,int qi,int qj){
+        //case 1 -completely non overlapping
+        if(qj<=si || qi>=sj){
+            return 0;//means the sum of current si and sj is not needed
+        }else if(si>=qi && sj<=qj){//complete overlap
+            return tree[i];
+
+        }else{
+            int mid=(si+sj)/2;
+            int left=getSumUtil(2*i+1, si, mid, qi, qj);
+            int right=getSumUtil(2*i+2, mid+1, sj, qi, qj);
+            return left+right;
+
+        }
+    }
+
+    public static int getSum(int arr[],int qi,int qj){
+        int n=arr.length;
+        return getSumUtil(0,0, n-1, qi, qj);
+    }
+
+
     //construction time complexity=O(n);
     //query and update time complexity logn
     public static void main(String[] args) {
@@ -27,9 +51,11 @@ public class SegmentTree {
         init(n);
         buildST(arr,0, 0, n-1);
 
-        for(int i=0;i<tree.length;i++){
-            System.out.print(tree[i]+" ");
-        }
+        // for(int i=0;i<tree.length;i++){
+        //     System.out.print(tree[i]+" ");
+        // }
+
+        System.out.println(getSum(arr,2,5));
 
     }
     
